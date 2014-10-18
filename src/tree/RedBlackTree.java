f1ee811ccb39b08ce46667cc20e7270eeb1b79a3
Giraudux <giraudux@gmail.com>
2014-10-18 19:56:23 +0200

    implements Collection

--- src/tree/RedBlackTree.java
+++ src/tree/RedBlackTree.java
@@ -3,7 +3,7 @@ package tree;
 /**
  * @author Alexis Giraudet
  */
-public class RedBlackTree extends BinaryTree {
+public class RedBlackTree<T> extends BinarySearchTree<T> {
     public static final boolean BLACK = false;
     public static final boolean RED = true;
 }

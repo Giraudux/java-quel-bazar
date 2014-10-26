48dc64ff205ec852dd8c31265c916c29cddab882
Giraudux <giraudux@gmail.com>
2014-10-26 22:50:35 +0100

    update test

---
+++ src/test/TreeTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import tree.RedBlackTree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * @author Alexis Giraudet
+ */
+//TODO: use junit
+public class TreeTest {
+    public static void Test0() {
+        int size = 1000;
+        ArrayList<Integer> a = new ArrayList<Integer>(size);
+        RedBlackTree<Integer> t = new RedBlackTree<Integer>();
+        for(int i=0; i<size; i++) {
+            a.add(i, i);
+        }
+        Collections.shuffle(a);
+
+        System.out.println("#addAll = "+t.addAll(a));
+        printTree(t);
+
+        System.out.println("#containsAll = "+t.containsAll(a));
+        printTree(t);
+
+        System.out.println("#removeAll = "+t.removeAll(a));
+        printTree(t);
+    }
+
+    private static <K extends Comparable<K>> void printTree(RedBlackTree<K> t) {
+        System.out.println("height = "+t.height());
+        System.out.println("size = "+t.size());
+        System.out.println("isEmpty = "+t.isEmpty());
+        System.out.println("content = "+t.toString());
+    }
+}

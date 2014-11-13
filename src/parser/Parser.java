6f765f5410aa29ff1556c9650a2614d715b2a579
Giraudux <giraudux@gmail.com>
2014-11-13 01:34:40 +0100

    update parser

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,32 +1,27 @@
 package parser;
 
+import bazar.Page;
+import tree.RedBlackTree;
+import tree.Tree;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StreamTokenizer;
-import java.util.Collection;
 
 /**
  * @author Alexis Giraudet
  */
 public class Parser {
-
-    public static void parseLines(String fileName, Collection<String> c) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            c.add(line.toLowerCase());
-        }
-    }
-
-    public static void parseTokens(String fileName, Collection<String> c) throws IOException {
+    public static Tree<String> parseDico(String fileName) throws IOException {
+        Tree<String> t = new RedBlackTree<String>();
         StreamTokenizer streamTokenizer = new StreamTokenizer(new BufferedReader(new FileReader(fileName)));
-        streamTokenizer.quoteChar('.');
+        /*streamTokenizer.quoteChar('.');
         streamTokenizer.quoteChar(',');
         streamTokenizer.quoteChar(';');
         streamTokenizer.quoteChar(':');
         streamTokenizer.quoteChar('!');
-        streamTokenizer.quoteChar(',');
+        streamTokenizer.quoteChar('?');
         streamTokenizer.quoteChar('"');
         streamTokenizer.quoteChar('(');
         streamTokenizer.quoteChar(')');
@@ -35,11 +30,44 @@ public class Parser {
         streamTokenizer.quoteChar('{');
         streamTokenizer.quoteChar('}');
         streamTokenizer.quoteChar('«');
-        streamTokenizer.quoteChar('»');
-        while(streamTokenizer.nextToken() != StreamTokenizer.TT_EOF) {
-            if(streamTokenizer.ttype == StreamTokenizer.TT_WORD) {
-                c.add(streamTokenizer.sval.toLowerCase());
+        streamTokenizer.quoteChar('»');*/
+        while (streamTokenizer.nextToken() != StreamTokenizer.TT_EOF) {
+            if (streamTokenizer.ttype == StreamTokenizer.TT_WORD) {
+                t.add(streamTokenizer.sval.toLowerCase());
             }
         }
+        return t;
+    }
+
+    public static Page parsePage(String fileName) throws IOException {
+        Page p;
+        Tree<String> words;
+        StreamTokenizer streamTokenizer = new StreamTokenizer(new BufferedReader(new FileReader(fileName)));
+        streamTokenizer.quoteChar('.');
+        streamTokenizer.quoteChar(',');
+        streamTokenizer.quoteChar(';');
+        streamTokenizer.quoteChar(':');
+        streamTokenizer.quoteChar('!');
+        streamTokenizer.quoteChar('?');
+        /*streamTokenizer.quoteChar('"');
+        streamTokenizer.quoteChar('(');
+        streamTokenizer.quoteChar(')');
+        streamTokenizer.quoteChar('[');
+        streamTokenizer.quoteChar(']');
+        streamTokenizer.quoteChar('{');
+        streamTokenizer.quoteChar('}');
+        streamTokenizer.quoteChar('«');
+        streamTokenizer.quoteChar('»');*/
+        if (streamTokenizer.nextToken() != StreamTokenizer.TT_NUMBER) {
+            throw new IOException();
+        }
+        p = new Page((int) streamTokenizer.nval);
+        words = p.getWords();
+        while (streamTokenizer.nextToken() != StreamTokenizer.TT_EOF) {
+            if (streamTokenizer.ttype == StreamTokenizer.TT_WORD) {
+                words.add(streamTokenizer.sval.toLowerCase());
+            }
+        }
+        return p;
     }
 }
